1e670369e3b63c27f00e72e5b97ded549f4d18f2
nicegentleorca-lang <nicegentleorca@gmail.com>
2026-09-06 20:51:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       manifest: {
         name: 'Deltoid',
         short_name: 'Deltoid',
-        description: 'Crash Courses and quizzes for MBBS preclinical students.',
+        description: 'Crash Course and quizzes for MBBS preclinical students.',
         theme_color: '#1B2A4A',
         background_color: '#EEF2F1',
         display: 'standalone',
